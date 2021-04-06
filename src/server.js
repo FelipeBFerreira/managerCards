a209d67aafe579e08013ab42a2aa6363fd213fe3
Felipe Bezerra <felipe.f3rreira@gmail.com>
2021-04-06 16:21:47 +0000

    Modified structure ejs

--- src/server.js
+++ src/server.js
@@ -6,9 +6,7 @@ const server = express();
 server.set("view engine", "ejs")
 server.use(express.static("public"));
 
-server.get("/" , function (req, res){
-
-    res.render("index");
-})
+server.get("/" , function (req, res){ res.render("pages/index"); })
+server.get("/signup" , function (req, res){ res.render("pages/signup"); })
 
 server.listen(3000, () => console.log('RODANDO'));
